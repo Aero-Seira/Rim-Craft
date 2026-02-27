83247def140236f6a10b23dfd9e941c958748b66
Aero Seira <145478933+Aero-Seira@users.noreply.github.com>
2026-02-28 02:39:58 +0800

    删除大部分与食物无关的kjs

---
+++ kubejs/startup_scripts/creative_tab/createdelight.js
@@ -0,0 +1,88 @@
+// 物品栏分类
+StartupEvents.modifyCreativeTab("createdelightcore:food", e => {
+  //食物
+  e.add([
+
+    // 原材料
+    'createdelight:corn_flour',
+    'createdelight:butter',
+    // 面团及其直接产物
+    'createdelight:dry_yeast',
+    'createdelight:oil_dough',
+    'createdelight:puff_pastry',
+    'createdelight:board_noodles',
+    'createdelight:vermicelli',
+    // 曲奇面团
+    'createdelight:persimmon_cookie_dough',
+    'createdelight:lemon_cookie_dough',
+    'createdelight:oatmeal_cookie_dough',
+    'createdelight:green_tea_cookie_dough',
+    'createdelight:cranberry_cookie_dough',
+    'createdelight:bayberry_cookie_dough',
+    'createdelight:chocolate_cookie_dough',
+    'createdelight:honey_cookie_dough',
+    'createdelight:sweet_berry_cookie_dough',
+    'createdelight:lime_cookie_dough',
+    'createdelight:chorus_cookie_dough',
+    'createdelight:bat_cookie_dough',
+    'createdelight:paw_cookie_dough',
+    // 未烤制玛芬
+    'createdelight:unbaked_red_velvet_cupcake',
+    'createdelight:unbaked_mixed_berry_muffin',
+    'createdelight:unbaked_chocolate_pumpkin_muffin',
+    'createdelight:unbaked_blueberry_muffin',
+    'createdelight:unbaked_cranberry_muffin',
+    'createdelight:unbaked_monster_muffin',
+    // 生食物
+    'createdelight:raw_empanada',
+    'createdelight:raw_calamari',
+    'createdelight:raw_ghast_calamari',
+    'createdelight:raw_cheese_pizza',
+    // 食物
+    'createdelight:wrapped_fries_ghasta',
+    'createdelight:oat_bread',
+    'createdelight:ketchup_corn_dog',
+    'createdelight:mayo_corn_dog',
+    'createdelight:salami',
+    'createdelight:fugu_roll',
+    'createdelight:empty_riceball',
+    'createdelight:yorkshire_pudding_and_beef',
+    'createdelight:boiling_water_cabbage',
+    'createdelight:braised_intestines_in_brown_sauce',
+    'createdelight:empty_popsicle',
+    'createdelight:enchanted_golden_lantern_fruit',
+    'createdelight:enchanted_golden_carrot',
+  ])
+})
+
+
+StartupEvents.modifyCreativeTab("createdelightcore:fluid", e => {
+  e.add([
+
+    // 食物相关流体
+    'createdelight:cake_batter_bucket',
+    'createdelight:egg_yolk_bucket',
+    'createdelight:artificial_egg_yolk_bucket',
+    'createdelight:soya_milk_bucket',
+    'createdelight:nut_milk_bucket',
+    'createdelight:vinegar_bucket',
+    'createdelight:yeast_bucket',
+    // 奶昔及对应冰激凌
+    'createdelight:vanilla_milkshake_bucket',
+    'createdelight:chocolate_milkshake_bucket',
+    'createdelight:strawberry_milkshake_bucket',
+    'createdelight:banana_milkshake_bucket',
+    'createdelight:mint_milkshake_bucket',
+    'createdelight:adzuki_milkshake_bucket',
+    'createdelight:lime_milkshake_bucket',
+    'createdelight:pomegranate_milkshake_bucket',
+    'createdelight:sweetberry_milkshake_bucket',
+    'createdelight:pumpkin_milkshake_bucket',
+    'createdelight:beetroot_milkshake_bucket',
+    'createdelight:apple_milkshake_bucket',
+    'createdelight:carrot_milkshake_bucket',
+    'createdelight:glow_berry_milkshake_bucket',
+    'createdelight:enhanced_fruit_milkshake_bucket',
+    // 特殊流体
+  ])
+})
